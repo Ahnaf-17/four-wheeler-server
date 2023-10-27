4051545af95a6d56cfb46d2340398974db732065
unknown <ahnaf.ahmed.42173@gmail.com>
2023-10-27 15:45:04 +0600

    delete complete

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const options = {
-            projection: {title:1, price:1,service_id:1}
+            projection: {title:1, price:1,service_id:1, img:1}
         }
         const result = await serviceCollection.findOne(query,options)
         res.send(result)
@@ -54,6 +54,25 @@ async function run() {
     app.post('/bookings', async(req,res)=>{
         const booking = req.body
         console.log(booking)
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result)
+    })
+
+    app.get('/bookings', async(req,res)=>{
+        console.log(req.query.email)
+        let query = {}
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    app.delete('/bookings/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await bookingCollection.deleteOne(query)
+        res.send(result)
     })
 
 
